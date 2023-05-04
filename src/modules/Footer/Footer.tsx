50b16f3ba8b0fee71dcb8b4bf3c36b49d90439d5
Marcin Grzmil <marcin.grzmil@hotmail.com>
2023-05-04 23:17:00 +0200

    added Header and Footer component

---
+++ src/modules/Footer/Footer.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react";
+import { Wrapper } from "../../components/Wrapper";
+
+export const Footer: FC = () => {
+  return <Wrapper>Footer</Wrapper>;
+};
---
+++ src/modules/Header/Header.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react";
+import { Wrapper } from "../../components/Wrapper";
+
+export const Header: FC = () => {
+  return <Wrapper>Header</Wrapper>;
+};
