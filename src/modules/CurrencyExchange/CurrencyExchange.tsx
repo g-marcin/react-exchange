30922c38b0fb0c3292ae83ccd7667d07e9360fb2
Marcin Grzmil <marcin.grzmil@hotmail.com>
2023-05-04 23:18:09 +0200

    added CurrencyExchange smart component

---
+++ src/modules/CurrencyExchange/CurrencyExchange.tsx
@@ -0,0 +1,22 @@
+import { FC, useState } from "react";
+import { ReactNode } from "react";
+import { Wrapper } from "../../components/Wrapper";
+import { currencyData } from "../../data";
+import { CurrencyDisplay } from "../CurrencyDisplay";
+import { CurrencyList } from "../CurrencyList";
+import { CurrencyType } from "../../types";
+
+type WrapperProps = {
+  children?: ReactNode | ReactNode[];
+  className?: string;
+};
+
+export const CurrencyExchange: FC<WrapperProps> = ({ className }) => {
+  const [presentCurrency, setPresentCurrency] = useState<CurrencyType | null>(null);
+  return (
+    <Wrapper className={className}>
+      <CurrencyDisplay presentCurrency={presentCurrency} />
+      <CurrencyList currencyData={currencyData} setPresentCurrency={setPresentCurrency} />
+    </Wrapper>
+  );
+};
