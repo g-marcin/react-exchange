5eb756ca383d2ce8a76dacd4cfb43c51623f7d07
Marcin Grzmil <marcin.grzmil@hotmail.com>
2023-05-08 19:15:42 +0200

    App.tsx imports clean-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,4 @@
-import { Header } from "./modules";
-import { CurrencyExchange } from "./modules/CurrencyExchange";
-import { Footer } from "./modules/Footer";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faTwitter, faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
-import {
-  faCaretDown,
-  faBars,
-  faDollarSign,
-  faEuroSign,
-  faZ,
-} from "@fortawesome/free-solid-svg-icons";
-
-library.add(faFacebook, faTwitter, faInstagram, faCaretDown, faBars, faDollarSign, faEuroSign, faZ);
+import { Header, CurrencyExchange, Footer } from "./modules";
 
 function App() {
   return (
