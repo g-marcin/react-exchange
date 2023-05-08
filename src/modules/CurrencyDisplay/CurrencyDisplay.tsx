84db59e22b01bc49931703a016161368d2feede7
Marcin Grzmil <marcin.grzmil@hotmail.com>
2023-05-08 19:21:38 +0200

    displayed currency to USD rate, displayed previous days info

--- src/modules/CurrencyDisplay/CurrencyDisplay.tsx
+++ src/modules/CurrencyDisplay/CurrencyDisplay.tsx
@@ -1,15 +1,36 @@
 import { FC } from "react";
-import { Wrapper } from "../../components/Wrapper";
-import { CurrencyType } from "../../types";
+import { Wrapper } from "../../components";
+import { CurrencyDisplayProps } from "../../types";
+import styles from "./currencyDisplay.module.css";
 
-type CurrencyDisplayProps = {
-  presentCurrency: CurrencyType | null;
-};
-
-export const CurrencyDisplay: FC<CurrencyDisplayProps> = ({ presentCurrency }) => {
+export const CurrencyDisplay: FC<CurrencyDisplayProps> = ({
+  presentCurrency,
+  fetchedCurrenciesHistory,
+}) => {
   return (
-    <Wrapper>
-      <p>{`present currency is ${JSON.stringify(presentCurrency)}`}</p>
+    <Wrapper className={styles["display__Wrapper"]}>
+      <p style={{ fontSize: "24px", margin: "auto" }}>
+        {presentCurrency
+          ? `Latest ${presentCurrency?.currencyCode} to USD rate: ${presentCurrency?.rate}`
+          : `Please choose your currency...`}
+      </p>
+      <p>
+        {fetchedCurrenciesHistory &&
+          Object.entries(fetchedCurrenciesHistory?.rates)
+            .map(([date, currencyRates]) => {
+              return (
+                <div className={styles["display__History"]}>
+                  <span>{date}</span>
+                  <span>
+                    {currencyRates
+                      ? JSON.stringify(currencyRates[presentCurrency?.currencyCode])
+                      : ""}
+                  </span>
+                </div>
+              );
+            })
+            .reverse()}
+      </p>
     </Wrapper>
   );
 };
