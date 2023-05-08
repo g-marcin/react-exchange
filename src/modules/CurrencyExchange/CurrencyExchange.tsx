af6170ede4d14cd3605f3e45bb11b4f18a2f4557
Marcin Grzmil <marcin.grzmil@hotmail.com>
2023-05-08 19:18:03 +0200

    added past data and currency names fetching logic

--- src/modules/CurrencyExchange/CurrencyExchange.tsx
+++ src/modules/CurrencyExchange/CurrencyExchange.tsx
@@ -2,7 +2,6 @@ import { FC, useState, useEffect, ReactNode, PropsWithChildren } from "react";
 import { httpClient } from "../../common";
 import { AxiosResponse } from "axios";
 import { Wrapper } from "../../components/Wrapper";
-import { currencyData } from "../../data";
 import { CurrencyDisplay } from "../CurrencyDisplay";
 import { CurrencyList } from "../CurrencyList";
 import { CurrencyType } from "../../types";
@@ -17,14 +16,17 @@ import {
   faZ,
 } from "@fortawesome/free-solid-svg-icons";
 library.add(faFacebook, faTwitter, faInstagram, faCaretDown, faBars, faDollarSign, faEuroSign, faZ);
-
-interface CurrencyExchangeProps extends PropsWithChildren {
-  className?: string;
-}
+import { FetchedCurrenciesType } from "../../types";
+import format from "date-fns/format";
+import subDays from "date-fns/subDays";
+import { CurrencyExchangeProps } from "../../types";
 
 export const CurrencyExchange: FC<CurrencyExchangeProps> = ({ className }) => {
+  const [currencyNames, setCurrencyNames] = useState<{ [k: string]: string }>({ null: "null" });
+  const [fetchedCurrencies, setFetchedCurrencies] = useState<FetchedCurrenciesType>(null);
   const [presentCurrency, setPresentCurrency] = useState<CurrencyType | null>(null);
-  const [fetchedCurrencies, setFetchedCurrencies] = useState([]);
+  const [fetchedCurrenciesHistory, setFetchedCurrenciesHistory] =
+    useState<FetchedCurrenciesType>(null);
 
   useEffect(() => {
     httpClient.get(`/latest?from=USD`).then((response: AxiosResponse) => {
@@ -33,14 +35,41 @@ export const CurrencyExchange: FC<CurrencyExchangeProps> = ({ className }) => {
     });
   }, []);
 
+  useEffect(() => {
+    const date = new Date();
+    const currentDate = format(date, "yyyy-MM-dd");
+    const weekAgoDate = format(subDays(date, 7), "yyyy-MM-dd");
+    httpClient.get(`/${weekAgoDate}..${currentDate}?from=USD`).then((response: AxiosResponse) => {
+      console.log(response.data);
+      setFetchedCurrenciesHistory(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    httpClient.get(`/currencies`).then((response: AxiosResponse) => {
+      console.log(response.data);
+      setCurrencyNames(response.data);
+    });
+  }, []);
+
   return (
     <Wrapper className={`${styles.wrapper} ${className}`}>
-      <CurrencyDisplay presentCurrency={presentCurrency} fetchedCurrencies={fetchedCurrencies} />
-      <CurrencyList currencyData={currencyData} currencyButtonHandler={currencyButtonHandler} />
+      <CurrencyDisplay
+        presentCurrency={presentCurrency}
+        fetchedCurrenciesHistory={fetchedCurrenciesHistory}
+      />
+      <CurrencyList
+        fetchedCurrencies={fetchedCurrencies}
+        currencyButtonHandler={currencyButtonHandler}
+        currencyNames={currencyNames}
+      />
     </Wrapper>
   );
 
-  function currencyButtonHandler(currency: CurrencyType) {
-    setPresentCurrency(currency);
+  function currencyButtonHandler(currencyCode: string): void {
+    setPresentCurrency({
+      currencyCode: currencyCode,
+      rate: fetchedCurrencies?.rates[`${currencyCode}`],
+    });
   }
 };
