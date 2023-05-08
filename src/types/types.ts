2a5bb2b311e63cc88260dc483a448aedd4acf246
Marcin Grzmil <marcin.grzmil@hotmail.com>
2023-05-08 19:09:13 +0200

    types moved to separate file

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,15 +1,45 @@
-import { IconName } from "@fortawesome/fontawesome-svg-core";
+import { PropsWithChildren } from "react";
 
 export type CurrencyType = {
-  name: string;
   currencyCode: string;
-  country: string;
-  countryCode: string | null;
-  exchangeRates: {
-    PLN?: number;
-    EUR?: number;
-    USD?: number;
-    JPY?: number;
-  };
-  iconName: IconName;
+  rate: number;
+};
+
+export type FetchedCurrenciesType = {
+  amount: number;
+  base: string;
+  date: string;
+  rates: rates;
+} | null;
+
+export type FetchedCurrenciesHistoryType = {
+  amount: number;
+  base: string;
+  start_date?: string;
+  end_date?: string;
+  rates: rates;
+} | null;
+
+export type rates = any;
+
+export type CurrencyDisplayProps = {
+  presentCurrency: CurrencyType | null;
+  fetchedCurrenciesHistory: FetchedCurrenciesHistoryType;
+};
+
+export interface CurrencyExchangeProps extends PropsWithChildren {
+  className?: string;
+}
+
+export type CurrencyProps = {
+  currencyCode: string;
+  currencyRate: any;
+  currencyButtonHandler: (currencyCode: string) => void;
+  currencyNames: { [k: string]: string };
+};
+
+export type CurrencyListProps = {
+  currencyButtonHandler: (currencyCode: string) => void;
+  fetchedCurrencies: FetchedCurrenciesType;
+  currencyNames: { [k: string]: string };
 };
