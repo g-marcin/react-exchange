5218302e58b825ec9d748740e82570ee50238d08
Marcin Grzmil <marcin.grzmil@hotmail.com>
2023-05-10 11:17:40 +0200

    added base currency selection feature

--- src/modules/CurrencyBase/CurrencyBase.tsx
+++ src/modules/CurrencyBase/CurrencyBase.tsx
@@ -1,17 +1,22 @@
 import { FC } from "react";
-import { Wrapper } from "../../components";
+import { Container, Wrapper } from "../../components";
 import { CurrencyBaseProps } from "../../types";
 import styles from "./currencyBase.module.css";
 
 export const CurrencyBase: FC<CurrencyBaseProps> = ({ currencyBaseHandler }) => {
   return (
-    <Wrapper className={styles["display__Wrapper"]}>
+    <Container className={styles["display__Wrapper"]}>
       <p style={{ fontSize: "24px", margin: "auto" }}> Please choose your base currency... </p>
-      <select name="" id="" onChange={(e) => currencyBaseHandler(e.target.value)}>
+      <select
+        className={styles["base__Select"]}
+        name=""
+        id=""
+        onChange={(e) => currencyBaseHandler(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="PLN">PLN</option>
       </select>
-    </Wrapper>
+    </Container>
   );
 };
