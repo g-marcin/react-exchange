4413be4786aa1ebecd3fd6b463bb74fbe6839533
Marcin Grzmil <marcin.grzmil@hotmail.com>
2023-05-11 15:35:21 +0200

    "restored currency base component files"

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
 import { WrapperProps } from "../../types";
-
 import styles from "./wrapper.module.css";
 
 export const Wrapper: FC<WrapperProps> = ({ className, children }, { ...delegated }) => {
