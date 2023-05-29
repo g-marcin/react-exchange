85416f1211d0f709854f0939d0efd5ef788d09c4
g-marcin <marcin.grzmil@hotmail.com>
2023-05-29 22:32:15 +0200

    es lint corr part2

--- src/components/CountryCodeSelect/CountryCodeSelect.tsx
+++ src/components/CountryCodeSelect/CountryCodeSelect.tsx
@@ -1,16 +1,33 @@
+import { FieldInputProps, FormikProps } from "formik";
 import { FC } from "react";
-import { FieldProps } from "formik";
-import Select from "react-select";
-import ReactSelectProps from "react-select";
-import Option from "react-select";
+import { default as Select } from "react-select";
 import styles from "./countryCodeSelect.module.css";
 
-export const FormikSelect: FC<ReactSelectProps & FieldProps> = ({ options, field, form }) => (
+// components/FormikReactSelect.tsx
+// define the OptionType for your app
+type MyOption = {
+  label: string;
+  value: string;
+};
+//define the group option type
+type GroupedOption = {
+  label: string; // group label
+  options: MyOption[];
+};
+// component props
+type Props = {
+options:MyOption[];
+field: FieldInputProps<any>,
+form:FormikProps<any>
+
+}
+
+export const FormikSelect:FC<Props> = ({ options, field, form }) => (
   <Select
     options={options}
     name={field.name}
-    value={options ? options.find((option) => option.value === field.value) : ""}
-    onChange={(option: Option) => form.setFieldValue(field.name, option.value)}
+    value={options ? options.find((option:MyOption) => option.value === field.value) : ""}
+    onChange={(option:any) => form.setFieldValue(field.name, option.value)}
     onBlur={field.onBlur}
     className={styles["select"]}
   />
