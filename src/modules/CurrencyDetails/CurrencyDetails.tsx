aea2bf457885d6e25e01254a6030b112c9e4bdbf
Marcin Grzmil <marcin.grzmil@hotmail.com>
2023-05-29 15:16:06 +0200

    added svg image to details page

--- src/modules/CurrencyDetails/CurrencyDetails.tsx
+++ src/modules/CurrencyDetails/CurrencyDetails.tsx
@@ -4,16 +4,20 @@ import { Card, Loader, ImageWithFallback } from "../../components";
 import { useParams } from "react-router-dom";
 import styles from "./currencyDetails.module.css";
 import { ReturnButton } from "../../components/ReturnButton";
+import { ReactComponent as GearSvg } from "../../assets/img/gearsvg.svg";
+import { useCountryDetails } from "../../hooks";
 
 export const CurrencyDetails: FC = () => {
   const { currencyCode } = useParams();
   const { fetchedCurrencyNames } = useContext(CurrencyContext);
+  console.log(useCountryDetails(currencyCode || ""));
 
   if (!currencyCode) {
     return <Loader />;
   }
   return (
-    <Card title={"Details"}>
+    <Card title={"Details"} className={styles["details"]}>
+      <GearSvg className={styles["gearsvg"]} />
       <label>Currency Code:</label>
       <p>{currencyCode}</p>
       <label>Currency Name:</label>
@@ -21,6 +25,7 @@ export const CurrencyDetails: FC = () => {
       <label>Currency Flag:</label>
       <ImageWithFallback currencyCode={currencyCode} className={styles["flag__wrapper"]} />
       <ReturnButton />
+      {/* TODO insert chart component and 1day/5day/week/month/3month/6month/year/full form */}
     </Card>
   );
 };
--- src/modules/CurrencyDisplay/CurrencyDisplay.tsx
+++ src/modules/CurrencyDisplay/CurrencyDisplay.tsx
@@ -1,15 +1,21 @@
-import { FC } from "react";
+import { FC, useContext } from "react";
 import { Wrapper } from "../../components";
+import { CurrencyContext } from "../../contexts";
 import { CurrencyBase } from "./CurrencyBase";
 import { CurrencyLatest } from "./CurrencyLatest";
 import { CurrencyHistory } from "./CurrencyHistory";
 import styles from "./currencyDisplay.module.css";
 
 export const CurrencyDisplay: FC = () => {
+  const { presentCurrency } = useContext(CurrencyContext);
   return (
     <Wrapper className={styles["display__Wrapper"]}>
-      <CurrencyLatest />
-      <CurrencyHistory />
+      {presentCurrency ? (
+        <>
+          <CurrencyLatest />
+          <CurrencyHistory />
+        </>
+      ) : null}
       <CurrencyBase />
     </Wrapper>
   );
