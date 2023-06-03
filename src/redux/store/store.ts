44761acb19d0b65b8e81f591a9b3bb10d9ca3da4
Marcin Grzmil <marcin.grzmil@hotmail.com>
2023-06-03 17:45:00 +0200

    redux-store configuration

---
+++ src/redux/store/store.ts
@@ -0,0 +1,15 @@
+import { Store, configureStore } from "@reduxjs/toolkit";
+import { currencyReducer } from "../currencySlice";
+import { frankfurterApiSlice } from "../apiSlice";
+import logger from "redux-logger";
+
+export const store: Store = configureStore({
+  reducer: {
+    currency: currencyReducer,
+    [frankfurterApiSlice.reducerPath]: frankfurterApiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(frankfurterApiSlice.middleware).concat(logger),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
