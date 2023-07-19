a9455be25ea2226efff97b0fc6f35f32dded84b8
Marcin Grzmil <marcin.grzmil@hotmail.com>
2023-07-19 23:11:19 +0200

    installed mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import { RouterProvider } from "react-router-dom";
-import { ReactQueryProvider } from "./common/ReactQuery";
-import { CurrencyContextProvider } from "./contexts";
-import { ThemeContextProvider } from "./contexts/ThemeContext";
-import { AppRouter } from "./routes";
+import CssBaseline from '@mui/material/CssBaseline';
+import { RouterProvider } from 'react-router-dom';
+import { ReactQueryProvider } from './common/ReactQuery';
+import { CurrencyContextProvider } from './contexts';
+import { ThemeContextProvider } from './contexts/ThemeContext';
+import { AppRouter } from './routes';
 
 function App() {
-  return (
-    <>
-      <ReactQueryProvider>
-        <CurrencyContextProvider>
-          <ThemeContextProvider>
-            <RouterProvider router={AppRouter} />
-          </ThemeContextProvider>
-        </CurrencyContextProvider>
-      </ReactQueryProvider>
-    </>
-  );
+    return (
+        <>
+            <ReactQueryProvider>
+                <CurrencyContextProvider>
+                    <ThemeContextProvider>
+                        <RouterProvider router={AppRouter} />
+                        <CssBaseline />
+                    </ThemeContextProvider>
+                </CurrencyContextProvider>
+            </ReactQueryProvider>
+        </>
+    );
 }
 
 export default App;
